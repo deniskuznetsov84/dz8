810649fa2d602a1545940b035889ebb59551ad60
Denys Kuznetsov <deniskuznetsov84@gmail.com>
2022-11-08 23:44:29 +0200

    Commit 1, test2 for py_charm.py

--- py_charm.py
+++ py_charm.py
@@ -5,3 +5,16 @@ def season(month):
         return 'Весна'
     elif month >= 6 and month <= 8:
         return 'Літо'
+
+def season(month):
+    if month in range(1, 3) or month == 12:
+        return 'Зима'
+    elif month in range(3, 6):
+        return 'Весна'
+    elif month in range(6, 9):
+        return 'Літо'
+    elif month in range(9, 12):
+        return 'Осінь'
+    else:
+        return 'Номер місяця повинен бути цілим числом від 1 до 12'
+
